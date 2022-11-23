7e6757902262f908f186a3d87ca7689c2675e641
Rachid Gaim Ahwaz <s183925@dtu.dk>
2022-11-23 04:52:46 +0100

    typo fix and added logo :D

--- app/src/main/java/com/example/chess/model/Block.kt
+++ app/src/main/java/com/example/chess/model/Block.kt
@@ -4,10 +4,11 @@ import androidx.compose.runtime.MutableState
 import com.example.chess.model.pieces.Piece
 
 //a block when initialized takes a piece object.
-//note that this is my atempt at a dependency injection
+//note that this is my attempt at a dependency injection
 class Block (givenPiece: MutableState<Piece?>) {
     val piece = givenPiece
     fun changePiece(newPiece: Piece?){
         piece.value = newPiece
     }
 }
+
